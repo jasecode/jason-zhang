8785d07222bcc0d61994917484e7fef4b0c73d89
jasecode <jzhang@mobify.com>
2021-02-08 23:09:46 -0800

    add galvanize to timeline

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -4,6 +4,7 @@ import sonyImg from  "../images/sony.jpg"
 import sfuImg from  "../images/sfu.jpg"
 import samsungImg from  "../images/samsung.png"
 import gsImg from  "../images/gossamer-threads.jpg"
+import galvImg from  "../images/galvanize.png"
 
 
 export default class Timeline extends Component {
@@ -22,10 +23,10 @@ export default class Timeline extends Component {
             <div className="row">
               <div className="col-md-12">
                 <div className="timeline-centered">
-                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
                     <div className="timeline-entry-inner">
                       <div className="timeline-icon color-3">
-                      <img class="img-circle img-responsive" src={mobifyImg} alt={resumeData['timeline'][0].alt}></img>
+                      <img class="img-circle img-responsive" src={galvImg} alt={resumeData['timeline'][0].alt}></img>
                       </div>
                       <div className="timeline-label">
                         <h2>{resumeData['timeline'][0].experience} <span> {resumeData['timeline'][0].date}</span></h2>
@@ -33,33 +34,33 @@ export default class Timeline extends Component {
                       </div>
                     </div>
                   </article>
-                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
                     <div className="timeline-entry-inner">
-                      <div className="timeline-icon color-4">
-                        <img class="img-circle img-responsive" src={sonyImg} alt={resumeData['timeline'][1].alt}></img>
+                      <div className="timeline-icon color-3">
+                      <img class="img-circle img-responsive" src={mobifyImg} alt={resumeData['timeline'][1].alt}></img>
                       </div>
                       <div className="timeline-label">
-                        <h2>{resumeData['timeline'][1].experience} <span>{resumeData['timeline'][1].date}</span></h2>
-                            {resumeData['timeline'][1].desc[0]}
-                            {resumeData['timeline'][1].desc[1]}
+                        <h2>{resumeData['timeline'][1].experience} <span> {resumeData['timeline'][1].date}</span></h2>
+                        <p>{resumeData['timeline'][1].desc}</p>
                       </div>
                     </div>
                   </article>
                   <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
                     <div className="timeline-entry-inner">
                       <div className="timeline-icon color-4">
-                        <img class="img-circle img-responsive" src={sfuImg} alt={resumeData['timeline'][2].alt}></img>
+                        <img class="img-circle img-responsive" src={sonyImg} alt={resumeData['timeline'][2].alt}></img>
                       </div>
                       <div className="timeline-label">
                         <h2>{resumeData['timeline'][2].experience} <span>{resumeData['timeline'][2].date}</span></h2>
-                        <p>{resumeData['timeline'][2].desc}</p>
+                            {resumeData['timeline'][2].desc[0]}
+                            {resumeData['timeline'][2].desc[1]}
                       </div>
                     </div>
                   </article>
                   <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
                     <div className="timeline-entry-inner">
                       <div className="timeline-icon color-4">
-                        <img class="img-circle img-responsive" src={samsungImg} alt={resumeData['timeline'][3].alt}></img>
+                        <img class="img-circle img-responsive" src={sfuImg} alt={resumeData['timeline'][3].alt}></img>
                       </div>
                       <div className="timeline-label">
                         <h2>{resumeData['timeline'][3].experience} <span>{resumeData['timeline'][3].date}</span></h2>
@@ -70,11 +71,22 @@ export default class Timeline extends Component {
                   <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
                     <div className="timeline-entry-inner">
                       <div className="timeline-icon color-4">
-                        <img class="img-circle img-responsive" src={gsImg} alt={resumeData['timeline'][4].alt}></img>
+                        <img class="img-circle img-responsive" src={samsungImg} alt={resumeData['timeline'][4].alt}></img>
                       </div>
                       <div className="timeline-label">
                         <h2>{resumeData['timeline'][4].experience} <span>{resumeData['timeline'][4].date}</span></h2>
                         <p>{resumeData['timeline'][4].desc}</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <img class="img-circle img-responsive" src={gsImg} alt={resumeData['timeline'][5].alt}></img>
+                      </div>
+                      <div className="timeline-label">
+                        <h2>{resumeData['timeline'][5].experience} <span>{resumeData['timeline'][5].date}</span></h2>
+                        <p>{resumeData['timeline'][5].desc}</p>
                       </div>
                     </div>
                   </article>
